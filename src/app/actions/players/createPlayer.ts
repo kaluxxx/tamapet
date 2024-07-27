3dc841e66ecd2d6f7a727429dc9e4aa43ab1a9bd
Lucas-allard <100565368+kaluxxx@users.noreply.github.com>
2024-07-27 17:05:31 +0200

    add debuging alert for telegram app

--- src/app/actions/players/createPlayer.ts
+++ src/app/actions/players/createPlayer.ts
@@ -5,6 +5,7 @@ import {PlayerDTO} from "@/types/dtos/playerDTO";
 import {Player} from "@/types/entities/player";
 
 export const createPlayer = async (playerDTO: PlayerDTO): Promise<Player> => {
+    alert('Creating player: ' + JSON.stringify(playerDTO));
     const player = await playerRepository.create(playerDTO);
 
     return JSON.parse(JSON.stringify(player));
